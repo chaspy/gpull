2a21ee2fda27127acd61f57a3ded5d465ce81d67
Takeshi Kondo <10370988+chaspy@users.noreply.github.com>
2024-01-22 23:33:27 +0900

    checkout and pull

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,50 @@
 use std::process::Command;
+use regex::Regex;
 
 fn main() {
-    let output = Command::new("git")
-        .arg("status")
+    // Get the current branch
+    let cur = Command::new("git")
+        .arg("rev-parse")
+        .arg("--abbrev-ref")
+        .arg("HEAD")
+        .output()
+        .expect("failed to execute command");
+    let current = String::from_utf8_lossy(&cur.stdout);
+
+    // Get the default branch
+    let default = Command::new("git")
+        .arg("remote")
+        .arg("show")
+        .arg("origin")
         .output()
         .expect("failed to execute command");
 
-    let output = String::from_utf8_lossy(&output.stdout);
+    let out = String::from_utf8_lossy(&default.stdout);
 
-    println!("{}", output)
+    let re = Regex::new(r"HEAD branch: (.*)").unwrap();
+    let cap = re.captures(&out).unwrap();
+    let default_branch = &cap[1];
+
+    // Checkout to the default branch
+    Command::new("git")
+        .arg("checkout")
+        .arg(default_branch)
+        .output()
+        .expect("failed to execute command");
+
+    // Pull the latest changes
+    Command::new("git")
+        .arg("pull")
+        .output()
+        .expect("failed to execute command");
+
+    // Back to the current branch
+    Command::new("git")
+        .arg("checkout")
+        .arg(current.to_string())
+        .output()
+        .expect("failed to execute command");
+
+
+
 }
